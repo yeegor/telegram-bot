9f44ddd7602150c68d1ad740b1677543084e347f
negzu <yegor.batov@gmail.com>
2020-02-16 19:48:54 +0200

    Add mongo connection string to config

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
+    CONNECTION_STRING: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@notedb-yunw7.mongodb.net/test?retryWrites=true&w=majority`,
     TOKEN: process.env.TOKEN,
     ERROR_LOG_PATH: process.env.ERROR_LOG_PATH,
     MESSAGE_LOG_PATH: process.env.MESSAGE_LOG_PATH
